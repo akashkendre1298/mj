6f80125d8ce46c687a5caac132218c4ff20e4e70
imdpc <prasadadhav22@gmail.com>
2024-01-19 13:09:06 +0530

    update Buttons.js

--- src/component/Photo/Buttons.js
+++ src/component/Photo/Buttons.js
@@ -8,22 +8,23 @@ import img8 from "./icons/diskette.png";
 import img9 from "./icons/search-in-browser-64.png";
 import img10 from "./icons/preview-48.png";
 import img11 from "./icons/delete.png";
-
+import close from "./icons/close_2997911.png";
 import "./Buttons.css";
 import EditImageTabList from "./../EditImageTabList/EditImageTabList";
 
 const Buttons = ({ onFileSelect }) => {
   // tablistconst [isPopupOpen, setIsPopupOpen] = useState(false);
   const [activeTab, setActiveTab] = useState(1);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const handleTabClick = (tabNumber) => {
     setActiveTab(tabNumber);
   };
   // close popup for edit image
 
-  // const handleClosePopup = () => {
-  //   setIsPopupOpen(false);
-  // };
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
 
   // Define custom tab names
   const tabNames = [
@@ -39,7 +40,6 @@ const Buttons = ({ onFileSelect }) => {
     "Overlay Image",
   ];
 
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [captionValue, setCaptionValue] = useState("");
   const fileInputRef = useRef(null);
   const uploadedFileRef = useRef(null);
@@ -69,7 +69,22 @@ const Buttons = ({ onFileSelect }) => {
     // Save the uploaded data (you can implement the saving logic here)
     console.log("Save data:", uploadedFileRef.current);
   };
+  const openPopup = () => {
+    setShowPopup(true);
+  };
 
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleInputSubmit = () => {
+    // Handle the input value as needed
+    console.log("Input value:", inputValue);
+    closePopup();
+  };
+  const handlePopupOpen = () => {
+    setIsPopupOpen(true);
+  };
   return (
     <>
       <input
@@ -91,74 +106,75 @@ const Buttons = ({ onFileSelect }) => {
         </li>
         <li>
           <a href="#file">
-            <div
-              className="Buttons-orderlist-to-adjust-images"
-              onClick={handlePopupOpen}
-            >
-              <img src={img2} alt="" />
+            <div className="Buttons-orderlist-to-adjust-images">
+              <img src={img2} alt="" onClick={handlePopupOpen} />
+              {isPopupOpen && (
+                <div className="edit-image-main-popup-container-css">
+                  {/* <EditImageTabList /> */}
+                  <div className="flex-container-for-tablist-for-edit-img">
+                    <div className="width-set-for-the-popup-windows-edit-image-section">
+                      {" "}
+                      <div className="edit-image-header-text-and-close-Button">
+                        <p className="edit-image-header-popup-window">
+                          Edit Image
+                        </p>
+
+                        <img
+                          src={close}
+                          className="edit-image-popup-close-image"
+                          onClick={handleClosePopup}
+                        />
+                      </div>
+                      <div className="flex">
+                        <div className="tab-list-buttons-aa-bb">
+                          {tabNames.map((tabName, index) => (
+                            <button
+                              key={index + 1}
+                              onClick={() => handleTabClick(index + 1)}
+                              className={
+                                activeTab === index + 1
+                                  ? "tab-list-active"
+                                  : "tab-list-button"
+                              }
+                            >
+                              {tabName}
+                            </button>
+                          ))}
+                          <div className="undo-redo-button-and-color-picker-combine">
+                            <section className="undeo-redo-btns-after-tablist">
+                              <button>Undo</button>
+                              <button>Redo</button>
+                            </section>
+                            <section className="color-picker-heading-and-color-picker">
+                              <p className="color-picker-heading">
+                                Current Color
+                              </p>
+                              <button className="color-pickkkker"></button>
+                            </section>
+                          </div>
+                        </div>
+                        <div className="tab-list-content">
+                          {tabNames.map((tabName, index) => (
+                            <div
+                              key={index + 1}
+                              className={
+                                activeTab === index + 1
+                                  ? "tab-list-pane tab-list-active"
+                                  : "tab-list-pane"
+                              }
+                            >
+                              Content for {tabName}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </a>
         </li>
-        {isPopupOpen && (
-          <div className="edit-image-main-popup-container-css">
-            {/* <EditImageTabList /> */}
-            <div className="flex-container-for-tablist-for-edit-img">
-              <div className="width-set-for-the-popup-windows-edit-image-section">
-                {" "}
-                <div className="edit-image-header-text-and-close-Button">
-                  <p className="edit-image-header-popup-window">Edit Image</p>
-
-                  <img
-                    src={close}
-                    className="edit-image-popup-close-image"
-                    onClick={handleClosePopup}
-                  />
-                </div>
-                <div className="flex">
-                  <div className="tab-list-buttons-aa-bb">
-                    {tabNames.map((tabName, index) => (
-                      <button
-                        key={index + 1}
-                        onClick={() => handleTabClick(index + 1)}
-                        className={
-                          activeTab === index + 1
-                            ? "tab-list-active"
-                            : "tab-list-button"
-                        }
-                      >
-                        {tabName}
-                      </button>
-                    ))}
-                    <div className="undo-redo-button-and-color-picker-combine">
-                      <section className="undeo-redo-btns-after-tablist">
-                        <button>Undo</button>
-                        <button>Redo</button>
-                      </section>
-                      <section className="color-picker-heading-and-color-picker">
-                        <p className="color-picker-heading">Current Color</p>
-                        <button className="color-pickkkker"></button>
-                      </section>
-                    </div>
-                  </div>
-                  <div className="tab-list-content">
-                    {tabNames.map((tabName, index) => (
-                      <div
-                        key={index + 1}
-                        className={
-                          activeTab === index + 1
-                            ? "tab-list-pane tab-list-active"
-                            : "tab-list-pane"
-                        }
-                      >
-                        Content for {tabName}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
 
         {/* <li>
           <a href="#file">
